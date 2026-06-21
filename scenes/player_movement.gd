9585de9d6d8b82bca44b85a4ebefa7ea85c8346b
Kanomus <146350033+Kanomus@users.noreply.github.com>
2026-06-21 13:43:28 +0530

    added player and war map scenes with basic movement

---
+++ scenes/player_movement.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const ROTATION_SPEED = 2.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction : Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * SPEED
+	move_and_slide()
+	
+	var rotate_direction : float = Input.get_axis("rotate_anticlockwise", "rotate_clockwise")
+	$Weapon.rotation_degrees += rotate_direction * ROTATION_SPEED
