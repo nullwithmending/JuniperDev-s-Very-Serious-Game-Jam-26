1c801245ccc22886a4bad5349239ba25a207e9dd
Kanomus <146350033+Kanomus@users.noreply.github.com>
2026-06-21 13:57:09 +0530

    forgot to add the other files :P

--- scenes/player_movement.gd
+++ scenes/player_movement.gd
@@ -1,10 +1,8 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const ROTATION_SPEED = 2.0
 const JUMP_VELOCITY = -400.0
-
 
 func _physics_process(delta: float) -> void:
 	var direction : Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
